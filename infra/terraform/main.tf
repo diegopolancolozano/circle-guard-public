7e3fd1c31eb89e273ddaae2eae6be2defe8ec907
diegopolancolozano <118563766+diegopolancolozano@users.noreply.github.com>
2026-05-05 13:03:08 -0500

    infra(terraform): support GKE-driven kubernetes provider (use_gke, cluster data sources)

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,5 +1,22 @@
+// Kubernetes provider: if use_gke=true, configure from GKE data sources (requires
+// GOOGLE_APPLICATION_CREDENTIALS or provider "google" configured). Otherwise use
+// the local kubeconfig file given by `kubeconfig_path`.
+data "google_client_config" "current" {}
+
+data "google_container_cluster" "cluster" {
+  count    = var.use_gke ? 1 : 0
+  name     = var.gke_cluster_name
+  location = var.gke_cluster_location
+  project  = var.gcp_project
+}
+
 provider "kubernetes" {
-  config_path = var.kubeconfig_path
+  # When using GKE we set host/token/ca; otherwise we load from kubeconfig_path.
+  host = var.use_gke ? "https://${data.google_container_cluster.cluster[0].endpoint}" : null
+  token = var.use_gke ? data.google_client_config.current.access_token : null
+  cluster_ca_certificate = var.use_gke ? base64decode(data.google_container_cluster.cluster[0].master_auth[0].cluster_ca_certificate) : null
+  config_path = var.use_gke ? null : var.kubeconfig_path
+  load_config_file = var.use_gke ? false : true
 }
 
 locals {
